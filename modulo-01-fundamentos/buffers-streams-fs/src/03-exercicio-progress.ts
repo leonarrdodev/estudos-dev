4d61ae3ede7c5bced4887ece17f49fb383732c05
Leonardo <leonardo.pro.7004@gmail.com>
2026-02-02 23:37:35 -0300

    feat(streams): implement file reading progress monitor & docs: add instructions

--- modulo-01-fundamentos/buffers-streams-fs/src/03-exercicio-progress.ts
+++ modulo-01-fundamentos/buffers-streams-fs/src/03-exercicio-progress.ts
@@ -1,10 +1,13 @@
 import { createReadStream, statSync, unlinkSync, writeFileSync } from "node:fs"
 
 const filePath = './backup.tmp'
+//cria arquivo de 20mb
 const content = Buffer.alloc(20 * 1024 * 1024, 'L')
 writeFileSync(filePath, content)
+//obtem o tamanho total do arquivo
 const fullSize = statSync(filePath).size
 
+//cria a stream de leitura
 const stream = createReadStream(filePath)
 
 let totalProcessed = 0
@@ -12,7 +15,7 @@ let totalProcessed = 0
 stream.on('data', (chunk: Buffer) => {
     totalProcessed += chunk.length
     const percentage = (totalProcessed / fullSize) * 100
-
+    //exibe no console atualizando a mesma linha
     process.stdout.write(`\r${percentage.toFixed(2)}%`)
 })
 
